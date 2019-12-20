915ecc335517d1e2ac9df161b676d882b31ef8c0
Liudmila Mzhachikh <l.mzhachikh@corp.mail.ru>
2019-12-20 16:28:23 +0300

    master Add material ui, make request to swagger hub

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function greeter(person: string) {
-  return "Hello, " + person;
-}
+const makeGetPetRequest = async(url = '') => {
+  try {
+    let response = await fetch(url, {
+      method: 'GET',
+      mode: 'cors',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    return await response.json();
+  }
+  catch(e) {
+    console.log(e);
+  }
+};
 
-function App() {
-  return (
-    <div className="App">
-      {greeter('Lucy')}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const makePostRequest = async(url = '', data = {}) => {
+//   try {
+//     let response = await fetch(url, {
+//       method: 'POST',
+//       mode: 'cors',
+//       cache: 'no-cache',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(data),
+//     });
+//     return await response.json();
+//   }
+//   catch(e) {
+//     console.log(e);
+//   }
+// };
+
+class App extends React.Component {
+  state = {
+    pet: {}
+  };
+
+  componentDidMount() {
+    makeGetPetRequest('https://virtserver.swaggerhub.com/cherries/cherries_petshop/1.0.0/pet/1')
+      .then(value => {
+        this.setState({ pet: value });
+      });
+  }
+
+  render () {
+    return (
+      <div className="App">
+        {Object.keys(this.state.pet).map(field => <div>{field}</div>)}
+      </div>
+    );
+  }
 }
 
 export default App;
